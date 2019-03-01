8072aa7ae3da0eb91b6b34099f77d0e68683b244
Petrik Lajos Szg <info@petrik.hu>
2019-03-01 13:32:29 +0100

    Alap program + konstruktorok

---
+++ JaratKezelo/JaratKezelo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaratKezelo
+{
+    public class JaratKezelo
+    {
+        string jaratSzam;
+        string repterHonnan;
+        string repterHova;
+        DateTime indulas;
+        int keses;
+
+
+        void ujJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            this.jaratSzam = jaratSzam;
+            this.repterHonnan = repterHonnan;
+            this.repterHova = repterHova;
+            this.indulas = indulas;
+            keses = 0;
+        }
+
+        void Keses(string jaratSzam, int keses)
+        {
+            this.jaratSzam = jaratSzam;
+            this.keses += keses;
+        }
+
+        void Keses(string jaratSam, TimeSpan keses)
+        {
+
+        }
+
+    }
+}
---
+++ JaratKezelo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JaratKezelo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JaratKezelo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa72c1c0-2f42-4696-85e1-7af4c3e325b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
